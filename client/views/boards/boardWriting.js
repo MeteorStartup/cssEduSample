22b9474f6ac64b4847f68d0a867ee02a3af83368
june <juner830310@gmail.com>
2016-10-12 17:38:45 +0900

    board count add

--- client/views/boards/boardWriting.js
+++ client/views/boards/boardWriting.js
@@ -22,8 +22,8 @@ Template.boardWriting.events({
     return history.go(-1);
   },
   'click [name=btnSave]': function(){
-    제목 = $('#제목').val();
-    내용 = $('#내용').val();
+    var 제목 = $('#제목').val();
+    var 내용 = $('#내용').val();
     console.log(제목);
     console.log(내용);
     if(제목.length <= 0) {
@@ -31,9 +31,16 @@ Template.boardWriting.events({
       $('#제목').focus();
       return;
     }
+    var 글번호;
+    if(CollectionBoards.find().count() > 0 ) {
+      글번호 = ((CollectionBoards.findOne({}, {sort: {createdAt: -1}}).글번호) || 0) + 1;
+    } else {
+      글번호 = 1;
+    }
     if(Router.current().route.getName() == 'boardWriting') {
       var obj = {
         createdAt: new Date(),
+        글번호: 글번호,
         제목: 제목,
         내용: 내용,
         작성자정보: Meteor.user()
